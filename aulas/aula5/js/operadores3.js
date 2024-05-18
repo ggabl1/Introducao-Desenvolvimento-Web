2f323a3f9eaea53f5ded120a09a0d30d142751c3
ggabl1 <gabigcastro2@gmail.com>
2024-05-18 11:22:00 -0300

    adicionando aula 5

---
+++ aulas/aula5/js/operadores3.js
@@ -0,0 +1,8 @@
+var ehdiferente = num1 != num2 // false
+var ehmenorouigual = num1 <= num2 // true
+var ehmaiorouigual = num1 >= num2 //
+var saiguais = 10 == "10" //true (verifica valor)
+var saoidenticos = 10 === "10" // false (verifica o tipo e valor)
+
+alert(saiguais)
+alert(saoidenticos)
---
+++ aulas/aula5/js/template_string.js
@@ -0,0 +1,5 @@
+var nome = "fulano"
+var idade = 10
+var endereco = Rua XPTO
+
+console.log(`seu nome é ${nome} vc tem ${idade} anos e mora no endereco ${endereco})
