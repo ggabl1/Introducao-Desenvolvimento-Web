06727dc2fe65fa84092addcf93372fbdbdd537fa
ggabl1 <gabigcastro2@gmail.com>
2024-05-25 11:58:08 -0300

    adicionando aula6

---
+++ aulas/aula6/js/array.js
@@ -0,0 +1,17 @@
+// arrays/listas/vetores
+//var nome = 'fulano' // variaveis simples
+var nomes = ['miguel', 'mateus', 'monica'] //variavel composta (array)
+//var idades = new array (tambem serve, mas é menos utilizado)
+console.log(nomes)
+
+nomes.push('davi', 'fulano', 'ciclano')// adiciona elementos no array
+console.log(nomes)
+console.log(nomes.lenght) // tamanho do  array
+nomes.pop()// por padrao remove o ultimo elemento do array
+console.log(nomes)
+nomes.reverse()// inverte o array
+console.log(nomes)
+nomes.shift()// por padrao remove o primeiro elemento do array
+console.log(nomes)
+nomes.sort() // ordena em ordem alfabetica
+console.log(nomes)
