aeffd32e2c85b9a42e977923ce9462f8c6617bef
ggabl1 <gabigcastro2@gmail.com>
2024-05-27 17:10:27 -0300

    adicionando lista 5

---
+++ listas/lista5/exercicio2/ex02.js
@@ -0,0 +1,5 @@
+var nome = "Ciclana"
+var idade = 80
+var empregoAtual = "Professora"
+var endereco = "Itaim"
+console.log(`Seu nome é ${nome} \nSua idade é ${idade} \nSua profissao é ${empregoAtual} \nSeu endereco é ${endereco}`)
