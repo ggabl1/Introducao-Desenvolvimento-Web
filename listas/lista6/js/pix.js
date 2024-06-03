80f0f70d9273830964e0cbc2eaabd545723d0a10
ggabl1 <gabigcastro2@gmail.com>
2024-06-02 22:48:21 -0300

    adicionando lista6

---
+++ listas/lista6/js/pix.js
@@ -0,0 +1,26 @@
+var Pix ={
+    chave: 12345678,
+    valor: 45.00,
+    dataTransacao: '03.06',
+    descricao:'academia',
+    pagador: [
+        {
+        nome: 'jessica',
+        documento: '34567890'
+        }
+    ],
+    recebedor: [
+        {
+            nome: 'aghata',
+            documento: '5784257325'
+        }
+    ],
+    historicoDeTransacoes: [
+        {
+        transacoes: [data, valor, descricao],
+        data: [14.03, 28.03, 25.05],
+        valor: [70.00, 90.00,65.99],
+        descricao: ['academia', 'gasolina', 'salao de beleza']
+        }
+    ]
+}
