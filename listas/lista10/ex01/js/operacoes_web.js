5cabd6391b81ce686622a32b0d9287ae1dfb2dfe
ggabl1 <gabigcastro2@gmail.com>
2024-06-28 21:49:46 -0300

    adicionando lista 10

---
+++ listas/lista10/ex01/js/operacoes_web.js
@@ -0,0 +1,12 @@
+export const function somar(n1, n2){
+    console.log(n1+n2)
+}
+export const function sub(n1, n2){
+    console.log(n1-n2)
+}
+export const function multiplicar(n1, n2){
+    console.log(n1*n2)
+}
+export const  function dividir(n1, n2){
+    console.log(n1/n2)
+}
---
+++ listas/lista10/ex01/js/script_web.js
@@ -0,0 +1,8 @@
+import { somar } from './operacoes_web'
+import { sub } from './operacoes_web'
+import { multiplicar } from './operacoes_web'
+import { dividir } from './operacoes_web'
+somar(3, 7)
+sub(7, 2)
+multiplicar(9, 4)
+dividir(6, 2)
